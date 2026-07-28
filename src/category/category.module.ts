295470b0f4c56a2a58a3e4a1c14c0c5304154216
Gabriel Moura Luz <gabrielmluz56@gmail.com>
2026-07-28 18:02:49 -0300

    fix(DI): user module in category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { ActiveUserPipe } from 'src/auth/pipes/active-user.pipe';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [CategoryController],
   providers: [CategoryService],
+  imports: [UserModule]
 })
 export class CategoryModule {}
